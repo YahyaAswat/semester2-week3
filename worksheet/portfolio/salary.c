dfe72bcdfad5e249e8e33f4edb35cc331be99265
YahyaAswat <hvkk0461@leeds.ac.uk>
2026-03-13 12:22:42 +0000

    Worksheet 2.3

--- worksheet/portfolio/salary.c
+++ worksheet/portfolio/salary.c
@@ -3,21 +3,31 @@
 
 /*
  * Potrfolio submission
- * Name:
- * ID:
+ * Name: Yahya H Aswat
+ * ID: 201961793
  */
 
  int main( void ) {
 
     // define and initialise variables for the problem data 
+    float salary = 30000;
+    float NI_rate = 9;
+    float tax_rate = 25;
+
+    float NI_deduction = (salary*NI_rate)/100;
+    float post_NI_deduction = salary - NI_deduction;
+    float taxable = post_NI_deduction - 12500;
+    float taxed = (taxable*tax_rate)/100;
+    float take_home_value = post_NI_deduction - taxed;
+
 
     // calculate the deductions and final take-home salary
 
     // Use only these print statement with appropriate formatting and variable names
-    //printf("Salary £...",var_name);
-    //printf("NI contribution £...",var_name);
-    //printf("Tax contribution £...",var_name);
-    //printf("Take home salary £...",var_name);
+    printf("Salary £%.2f\n", salary);
+    printf("NI contribution £%.2f\n",NI_deduction);
+    printf("Tax contribution £%.2f\n",taxed);
+    printf("Take home salary £%.2f\n",take_home_value);
 
     return 0;
  }
